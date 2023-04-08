aed115fd04949584580bf5461ccd3034feed8a75
catalincd <cc.dumitrescu02@gmail.com>
2023-04-08 18:47:13 +0300

    Minimal Interface

---
+++ src/frontend/packets/packets.js
@@ -0,0 +1,104 @@
+var hexColumnsElement
+var hexValuesElement
+var headersElement
+
+var activePacketIdx = -1
+
+
+const selectPacketElement = (element) => {
+    selectPacket(element.dataset.packetid, element)
+}
+
+const buf2hex = (buffer) => [...new Uint8Array(buffer)].map(x => x.toString(16).padStart(2, '0'))
+
+
+const getNum = (num) => {
+    var str = ""
+    var added = 3 - num.toString().length
+    for (var i = 0; i < added; i++)
+        str += "0"
+    str += num.toString() + "0"
+    return str
+}
+
+
+const selectPacket = (idx, activeElement) => {
+
+    const allPacketElements = document.getElementsByClassName("packetLine")
+    for (var i = 0; i < allPacketElements.length; i++)
+        allPacketElements[i].classList.remove("active")
+
+    activeElement.classList.add("active")
+
+    hotSelectPacket(idx)
+}
+
+const hotSelectPacket = (idx) => {
+    activePacketIdx = idx
+    hexColumnsElement = document.getElementById("hexNumColumn")
+    hexValuesElement = document.getElementById("hexValues")
+
+    headersElement = document.getElementById("headerValues")
+
+    const data = getCurrentTabData()
+    const thisPacket = data.allPackets[idx]
+    console.log(data)
+    setCurrentPacketSelected(idx)
+
+
+    //hex
+    var hexArray = buf2hex(thisPacket.data)
+    var cnt = 0
+    var hexNumHTML = ""
+    var hexValueHTML = ""
+
+
+    while (hexArray.length > 0) {
+        hexNumHTML += `<span>${getNum(cnt)}</span>`
+
+        currentHexArray = hexArray.slice(0, 16)
+        hexArray = hexArray.slice(16)
+
+        hexValueHTML += currentHexArray.map(hex => `<span>${hex}</span>`).join('')
+
+        cnt++
+    }
+
+    hexColumnsElement.innerHTML = hexNumHTML
+    hexColumnsElement.style.display = "grid"
+
+    hexValuesElement.innerHTML = hexValueHTML
+
+    //headers
+
+    var headersHTML = ""
+
+    headersHTML += "<p>Ethernet II</p>"
+    headersHTML += `<ul class="headersList">
+                        <li>Destination: ${thisPacket.ether.destination}</li>
+                        <li>Source: ${thisPacket.ether.source}</li>
+                        <li>Type: ${matchType(thisPacket.ether.type)}</li>
+                    </ul>`
+
+    headersHTML += "<br><p>IPv4</p>"
+    headersHTML += `<ul class="headersList">
+                        <li>Destination: ${thisPacket.ipvx.destination}</li>
+                        <li>Source: ${thisPacket.ipvx.source}</li>
+                    </ul>`
+
+    headersElement.innerHTML = headersHTML
+}
+
+
+const matchType = (type) => {
+    if (type == "0800")
+        return "IPv4 (0x0800)"
+
+    return "UKNWN"
+}
+
+
+
+window.addEventListener("load", async () => {
+    //print cox
+})
---
+++ src/frontend/util/htmlparser.js
@@ -0,0 +1,105 @@
+const getHtmlFromData = (data) => {
+    console.log(data)
+
+    var packetsHtml = ""
+
+    if (data.allPackets.length <= 0) {
+        console.log("COX FILE NO PACKETS")
+        return
+    }
+
+    const originalSeconds = data.allPackets[0].header.timestampSeconds
+
+    for (var i = 0; i < data.allPackets.length; i++) {
+        packetsHtml += packetToHtml(data.allPackets[i], i, originalSeconds)
+    }
+
+
+    const tableHead = `<div id="packetTableHead">
+                            <div class="packetCell tableHead">
+                                <p class="packetNum">Nr</p>
+                            </div>
+                            <div class="packetCell tableHead">
+                                <p class="packetTime">Timestamp</p>
+                            </div>
+                            <div class="packetCell tableHead">
+                                <p class="packetSrc">Source</p>
+                            </div>
+                            <div class="packetCell tableHead">
+                                <p class="packetDest">Destination</p>
+                            </div>
+                            <div class="packetCell tableHead">
+                                <p class="packetProtocol">Protocol</p>
+                            </div>
+                            <div class="packetCell tableHead">
+                                <p class="packetLength">Length</p>
+                            </div>
+                        </div>`
+
+    return `<div id="mainGrid">
+                <div id="packetsWrapper">
+                    <div id="packetsTableWrapper" class="wrapper">
+                        ${tableHead}
+                        <div id="packetsTable">
+                            <div id="tableTopGradient"></div>
+                            ${packetsHtml}
+                            <div id="tableBottomGradient"></div>
+                        </div>
+                    </div>
+                    <div id="packetsScrollBarWrapper">
+                        <div id="packetsScrollBar"></div>
+                    </div>
+                </div>
+                <div id="detailsWrapper">
+                    <div id="headersWrapper">
+                        <div id="headerValues"></div>
+                    </div>
+                    <div id="hexWrapper">
+                        <div id="hexNumColumn"></div>
+                        <div id="hexValues"></div>
+                    </div>
+                </div>
+            </div>`
+}
+
+
+const packetToHtml = (packet, num, originalSeconds) => {
+    return `<div class="packetLine" data-packetid="${num}" onclick="selectPacketElement(this)" style="animation-delay: ${Math.min(num * 35, 1000)}ms" data-idx="${num}">
+                <div class="packetCell">
+                    <p class="packetNum">${num + 1}</p>
+                </div>
+                <div class="packetCell">
+                    <p class="packetTime">${microToSeconds(packet.header.timestampMicroseconds, packet.header.timestampSeconds, originalSeconds)}</p>
+                </div>
+                <div class="packetCell">
+                    <p class="packetSrc">${packet.ipvx.source}</p>
+                </div>
+                <div class="packetCell">
+                    <p class="packetDest">${packet.ipvx.destination}</p>
+                </div>
+                <div class="packetCell">
+                    <p class="packetProtocol">${"TCP"}</p>
+                </div>
+                <div class="packetCell">
+                    <p class="packetLength">${packet.header.capturedLength}</p>
+                </div>
+
+            </div>`
+}
+
+const reparseHtml = (html, { animated }) => {
+
+    var parsedHtml = html
+
+    if (!animated) {
+        parsedHtml = html.replace(/packetLine/g, "packetLine static")
+    }
+
+    return parsedHtml
+}
+
+
+const microToSeconds = (micro, seconds, original) => {
+
+    return ((seconds - original) + (micro / 1000000)).toFixed(3)
+}
